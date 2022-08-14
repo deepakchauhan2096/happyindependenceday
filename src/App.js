bada1a7cee0f7ae4584de53260c58491bb915cf4
deepakchauhan2096 <deepakchauhan2096@gmail.com>
2022-08-15 01:28:26 +0530

    ff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import ReactAudioPlayer from 'react-audio-player';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <ReactAudioPlayer
+        src="jaanpe.mp3"
+        autoPlay
+        loop
+      />
+      <div id="pole"></div>
+      <div className="saffron" onclick="sImp()"></div>
+      <div className="white" onclick="wImp()"></div>
+      <div className="green" onclick="gImp()"></div>
+      <div onclick="cImp()">
+        <div className="chakra"></div>
+        <div id="one"></div>
+        <div id="two"></div>
+        <div id="three"></div>
+        <div id="four"></div>
+        <div id="five"></div>
+        <div id="six"></div>
+        <div id="seven"></div>
+        <div id="eight"></div>
+        <div id="nine"></div>
+        <div id="ten"></div>
+        <div id="eleven"></div>
+        <div id="twelve"></div>
+      </div>
+      <br /><br />
+      <p className="font1"> वंदे मातरम, Vande  Mataram</p>
+      <br />
+      <p></p>
+      <img src="India_flag-XL-anim.gif" alt="india" style={{ margin: "0 auto", display: "flex" }} />
+
+
+      <h1 id="text">Happy Independence Day</h1>
+    </div >
   );
 }
 
